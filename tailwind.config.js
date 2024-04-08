e5d3afe2826d7788e50dbc12c1fa284083cea7b6
Andrew Ariel Fandy Tuyu <andrewfandy13@gmail.com>
2024-04-08 16:59:58 +0700

    Changes some lines

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
+      colors:{
+        "primary-cyan": "hsl(180, 66%, 49%)",
+        "primary-dark-violet": "hsl(257, 27%, 26%)",
+        "secondary-red": "hsl(0, 87%, 67%)",
+        "neutral-gray": "hsl(0, 0%, 75%)",
+        "neutral-gray-violet": "hsl(257, 7%, 63%)",
+        "neutral-dark-blue": "hsl(255, 11%, 22%)",
+        "neutral-dark-violet": "hsl(260, 8%, 14%)"
+
+      }
     },
   },
   plugins: [],
